ef7413b92ba78ad5e2f030df00df26e87dda5b18
William Mosto <will.mosto@gmail.com>
2021-07-27 19:57:48 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+//newHeader.setAttribute('id','victory');
+document.body.appendChild(newHeader);
+victory.innerHTML = "<h1>Will is the champion</h1>";
